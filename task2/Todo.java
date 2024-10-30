b55ced4cd01ea030fc04e2e48f5075baa83956bb
kiri7aya <157214916+kiri7aya@users.noreply.github.com>
2024-10-30 16:27:16 +0500

    Create Todo.java

---
+++ task2/Todo.java
@@ -0,0 +1,28 @@
+public class Todo {
+    private int id;
+    private String title;
+    private boolean completed;
+
+    public Todo(int id, String title, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
