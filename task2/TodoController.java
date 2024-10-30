92e1d37fd98882387f3137ed38455dc226c62a92
kiri7aya <157214916+kiri7aya@users.noreply.github.com>
2024-10-30 17:34:17 +0500

    Update TodoController.java

--- task2/TodoController.java
+++ task2/TodoController.java
@@ -1,27 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoController {
-    private TodoView view;
-    private List<Todo> todos;
+    private final TodoView view;
+    private final List<Todo> todos;
 
     public TodoController(TodoView view) {
         this.view = view;
         this.todos = new ArrayList<>();
-        this.view.setAddTodoListener(this::addTodo);
+        this.view.setController(this);
     }
 
-    private void addTodo() {
+    public void addTodo() {
         String title = view.getNewTodoTitle();
         if (!title.isEmpty()) {
-            Todo newTodo = new Todo(todos.size(), title, false);
+            Todo newTodo = new Todo(todos.size(), title);
             todos.add(newTodo);
             view.displayTodos(todos);
         }
-    }
-}
-
-// Main
-public class Main {
-    public static void main(String[] args) {
-        TodoView view = new TodoViewImpl();
-        new TodoController(view);
     }
 }
