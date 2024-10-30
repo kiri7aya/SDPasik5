b73f2045809e7ba3ece68aeff6e312aecd1cfde4
kiri7aya <157214916+kiri7aya@users.noreply.github.com>
2024-10-30 16:31:03 +0500

    Create Student.java

---
+++ task3/Student.java
@@ -0,0 +1,28 @@
+public class Student {
+    private String name;
+    private int age;
+    private String course;
+
+    public Student(String name, int age, String course) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
